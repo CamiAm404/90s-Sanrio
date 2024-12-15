75de622a41041afa98fc5a110746bfaa407f1baf
Camila Amaro <camilaamarov@yahoo.es>
2024-12-14 21:24:49 -0500

    feat: Add gifs page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import {HomePageComponent} from './public/views/pages/home-page/home-page.component';
 import {CharactersPageComponent} from './public/views/pages/characters-page/characters-page.component';
 import {DrawingsPageComponent} from './public/views/pages/drawings-page/drawings-page.component';
+import {GifsPageComponent} from './public/views/pages/gifs-page/gifs-page.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home-page', pathMatch: 'full'},
   {path: 'home-page', component: HomePageComponent},
   {path: 'characters', component: CharactersPageComponent},
-  {path: 'drawings', component: DrawingsPageComponent}
+  {path: 'drawings', component: DrawingsPageComponent},
+  {path: 'gifs', component: GifsPageComponent}
   // {path: '**', component: PageNotFoundComponent}
 ];
---
+++ src/app/public/views/pages/gifs-page/gifs-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-gifs-page',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './gifs-page.component.html',
+  styleUrl: './gifs-page.component.css'
+})
+export class GifsPageComponent {
+
+}
